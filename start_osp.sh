9589d258facad0519b0c2a99033562b22c4dc9a9
Fabián Aravena <fabian@aravena.dev>
2021-06-21 22:14:28 -0400

    Fix

--- start_osp.sh
+++ start_osp.sh
@@ -3,7 +3,7 @@ echo "*******************************************************"
 echo "  Iniciando Servidor de Quake III Arena "
 echo "      Script by MeLkOr "
 echo "*******************************************************"
-./quake3e.ded.x64 +set com_hunkmegs 128 +set fs_game osp +set dedicated 2 +set cl_punkbuster 0 $@
+./quake3e.ded.x64 +set com_hunkmegs 128 +set fs_game osp +set gamename osp +set dedicated 2 +set cl_punkbuster 0 $@
 echo ""
 echo "*******************************************************"
 echo "  FIN - SERVIDOR Q3A DETENIDO"
